df68306aabc0c999e9b8fc41d22bc6ec1b0f8c54
Teyson <xm729905@iteso.mx>
2021-02-15 01:50:18 -0600

    AES128 functions updated

--- frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.c
+++ frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.c
@@ -12,8 +12,7 @@
 #define SERVER_PORT 10000
 #define SOCKET_TIMEOUT = 20
 
-
-uint8_t test_string[] = {"01234567890123456789"};
+//#if LWIP_NETCONN
 
 /*AES variables*/
 uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
@@ -24,10 +23,32 @@ struct AES_ctx ctx;
 CRC_Type *base = CRC0;
 uint32_t checksum32;
 
+/*!
+ * @brief Init for CRC-32.
+ * @details Init CRC peripheral module for CRC-32 protocol.
+ *          width=32 poly=0x04c11db7 init=0xffffffff refin=true refout=true xorout=0xffffffff check=0xcbf43926
+ *          name="CRC-32"
+ *          http://reveng.sourceforge.net/crc-catalogue/
+ */
+static void InitCrc32(CRC_Type *base, uint32_t seed)
+{
+    crc_config_t config;
+
+    config.polynomial         = 0x04C11DB7U;
+    config.seed               = seed;
+    config.reflectIn          = true;
+    config.reflectOut         = true;
+    config.complementChecksum = true;
+    config.crcBits            = kCrcBits32;
+    config.crcResult          = kCrcFinalChecksum;
+
+    CRC_Init(base, &config);
+}
 
 /*Encrypt message using AES128 algorithm*/
 uint8_t encrypt(uint8_t *message)
 {
+
 	size_t message_len, padded_len;
 	uint8_t padded_msg[512] = {0};
 
@@ -39,22 +60,50 @@ uint8_t encrypt(uint8_t *message)
 	padded_len = message_len + (16 - (message_len%16) );
 	memcpy(padded_msg, message, message_len);
 
+	PRINTF("Message: ");
+			for(int i=0; i<padded_len; i++) {
+				PRINTF("0x%02x,", padded_msg[i]);
+			}
+	PRINTF("\r\n");
+
 	AES_CBC_encrypt_buffer(&ctx, padded_msg, padded_len);
 
-	return padded_msg;
+	PRINTF("Encrypted Message: ");
+		for(int i=0; i<padded_len; i++) {
+			PRINTF("0x%02x,", padded_msg[i]);
+		}
+	PRINTF("\r\n");
+
+	decrypt(padded_msg);
+
+	return *padded_msg;
+
 }
 
-
+/*Decrypt message using AES128 algorithm*/
 uint8_t decrypt(uint8_t *message)
 {
+
 	size_t message_len;
-	uint8_t decrypted_msg[512];
+	uint8_t decrypted_msg[512]= {0};
+
+	/* Init the AES context structure */
+	AES_init_ctx_iv(&ctx, key, iv);
 
 	message_len = strlen(message);
 	memcpy(decrypted_msg, message, message_len);
 
+    /*Decrypt message*/
 	AES_CBC_decrypt_buffer(&ctx, decrypted_msg, message_len);
 
-	return decrypted_msg;
+	PRINTF("Decrypted Message: ");
+	for(int i=0; i<message_len; i++) {
+		PRINTF("0x%02x,", decrypted_msg[i]);
+		}
+	PRINTF("\r\n");
+
+	return *decrypted_msg;
 }
+
+
 
--- frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.h
+++ frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.h
@@ -2,7 +2,7 @@
  * ssnet.h
  *
  *  Created on: 13 feb. 2021
- *      Author: luist
+ *      Author: Luis Teyssier
  */
 
 #ifndef SSNET_H_
@@ -17,10 +17,16 @@
 #include "aes.h"
 #include "fsl_crc.h"
 
+#include "lwip/opt.h"
+
+
+#include "lwip/sys.h"
+#include "lwip/api.h"
 
 
 uint8_t encrypt(uint8_t *message);
 uint8_t decrypt(uint8_t *message);
+//static void tcpecho_ssnet_thread(void *arg);
 
 
 #endif /* SSNET_H_ */
