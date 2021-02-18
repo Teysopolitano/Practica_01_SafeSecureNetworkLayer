eee4a9c2ac4289d60346e13a9ab286f2ba785ec9
Teyson <xm729905@iteso.mx>
2021-02-17 22:10:12 -0600

    Updated functions. Encryption function not working at all

--- frdmk64f_lwip_tcpecho_freertos_myssn/source/lwip_tcpecho_freertos.c
+++ frdmk64f_lwip_tcpecho_freertos_myssn/source/lwip_tcpecho_freertos.c
@@ -28,6 +28,7 @@
 
 #include "aes.h"
 #include "fsl_crc.h"
+#include "ssnet.h"
 
 /*******************************************************************************
  * Definitions
@@ -72,7 +73,7 @@
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-void aescrc_test_task(void *arg);
+//void aescrc_test_task(void *arg);
 
 /*******************************************************************************
  * Variables
@@ -130,9 +131,9 @@ int main(void)
            ((u8_t *)&netif_gw)[2], ((u8_t *)&netif_gw)[3]);
     PRINTF("************************************************\r\n");
 
-    tcpecho_init();
-
-    sys_thread_new("aescrc_task", aescrc_test_task, NULL, 1024, 4);
+//    tcpecho_init();
+    sys_thread_new("tcpecho_task", tcpecho_server_ssnet_thread, NULL, 1024, 4);
+//    sys_thread_new("aescrc_task", aescrc_test_task, NULL, 1024, 4);
 
     vTaskStartScheduler();
 
@@ -141,69 +142,47 @@ int main(void)
 }
 #endif
 
-/*!
- * @brief Init for CRC-32.
- * @details Init CRC peripheral module for CRC-32 protocol.
- *          width=32 poly=0x04c11db7 init=0xffffffff refin=true refout=true xorout=0xffffffff check=0xcbf43926
- *          name="CRC-32"
- *          http://reveng.sourceforge.net/crc-catalogue/
- */
-static void InitCrc32(CRC_Type *base, uint32_t seed)
-{
-    crc_config_t config;
 
-    config.polynomial         = 0x04C11DB7U;
-    config.seed               = seed;
-    config.reflectIn          = true;
-    config.reflectOut         = true;
-    config.complementChecksum = true;
-    config.crcBits            = kCrcBits32;
-    config.crcResult          = kCrcFinalChecksum;
-
-    CRC_Init(base, &config);
-}
-
-void aescrc_test_task(void *arg)
-{
-
-	uint8_t test_string[] = {"01234567890123456789"};
-	/* AES data */
-	uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
-	uint8_t iv[]  = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-	struct AES_ctx ctx;
-	size_t test_string_len, padded_len;
-	uint8_t padded_msg[512] = {0};
-	/* CRC data */
-	CRC_Type *base = CRC0;
-	uint32_t checksum32;
-
-
-	PRINTF("AES and CRC test task\r\n");
-
-	PRINTF("\nTesting AES128\r\n\n");
-	/* Init the AES context structure */
-	AES_init_ctx_iv(&ctx, key, iv);
-
-	/* To encrypt an array its lenght must be a multiple of 16 so we add zeros */
-	test_string_len = strlen(test_string);
-	padded_len = test_string_len + (16 - (test_string_len%16) );
-	memcpy(padded_msg, test_string, test_string_len);
-
-	AES_CBC_encrypt_buffer(&ctx, padded_msg, padded_len);
-
-	PRINTF("Encrypted Message: ");
-	for(int i=0; i<padded_len; i++) {
-		PRINTF("0x%02x,", padded_msg[i]);
-	}
-	PRINTF("\r\n");
-
-
-	PRINTF("\nTesting CRC32\r\n\n");
-
-    InitCrc32(base, 0xFFFFFFFFU);
-    CRC_WriteData(base, (uint8_t *)&padded_msg[0], padded_len);
-    checksum32 = CRC_Get32bitResult(base);
-
-    PRINTF("CRC-32: 0x%08x\r\n", checksum32);
-
-}
+//void aescrc_test_task(void *arg)
+//{
+//
+////	uint8_t test_string[] = {"01234567890123456789"};
+//	uint8_t test_string[] = {"hello"};
+//	size_t test_string_len; //padded_len;
+//	//uint8_t padded_msg[512] = {0};
+//	uint8_t *enc_msg, *dec_msg;
+//
+//	/* CRC data */
+//	//CRC_Type *base = CRC0;
+//	uint32_t checksum32;
+//
+//
+//	PRINTF("AES and CRC test task\r\n");
+//
+//	PRINTF("\nTesting AES128\r\n\n");
+//
+////	/* To encrypt an array its lenght must be a multiple of 16 so we add zeros */
+//	test_string_len = strlen(test_string);
+////	padded_len = test_string_len + (16 - (test_string_len%16) );
+////	memcpy(padded_msg, test_string, test_string_len);
+//
+//	enc_msg = encrypt(test_string);
+//
+//	PRINTF("\r\n");
+//	PRINTF("\nTesting CRC32\r\n\n");
+//
+//    /*Calculate CRC32*/
+//	checksum32 = calculate_crc32(enc_msg);
+//
+//    PRINTF("CRC-32: 0x%08x\r\n", checksum32);
+//
+//   /*Decipher message */
+//    dec_msg = decrypt(enc_msg);
+//
+//    PRINTF("Deciphered Message: ");
+//    		for(int i=0; i<test_string_len; i++) {
+//    			PRINTF("0x%02x,", dec_msg[i]);
+//    			}
+//    PRINTF("\r\n");
+//
+//}
--- frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.c
+++ frdmk64f_lwip_tcpecho_freertos_myssn/source/ssnet.c
@@ -19,7 +19,8 @@ struct AES_ctx ctx;
 
 /* CRC data */
 CRC_Type *base = CRC0;
-static uint32_t checksum32;
+uint32_t checksum32;
+
 
 /*!
  * @brief Init for CRC-32.
@@ -81,7 +82,9 @@ uint8_t * decrypt(uint8_t *message)
 {
 
 	size_t message_len;
-	static uint8_t decrypted_msg[512]= {0};
+	static uint8_t decrypted_msg[512];
+
+//	p_dec_msg = &decrypted_msg;
 
 	/* Init the AES context structure */
 	AES_init_ctx_iv(&ctx, key, iv);
@@ -89,7 +92,7 @@ uint8_t * decrypt(uint8_t *message)
 	message_len = strlen(message);
 	memcpy(decrypted_msg, message, message_len);
 
-    /*Decrypt message*/
+	/*Decrypt message*/
 	AES_CBC_decrypt_buffer(&ctx, decrypted_msg, message_len);
 
 	PRINTF("Decrypted Message: ");
@@ -101,90 +104,184 @@ uint8_t * decrypt(uint8_t *message)
 	return decrypted_msg;
 }
 
-uint32_t calculate_crc32(uint8_t *message)
-{
-
-	size_t padded_len;
-
-	padded_len = strlen(message);
-
-	InitCrc32(base, 0xFFFFFFFFU);
-	CRC_WriteData(base, (uint8_t *)&message[0], padded_len);
-	checksum32 = CRC_Get32bitResult(base);
-
-	return checksum32;
-
-}
-
-//void tcpecho_ssnet_thread(void *arg)
+//uint32_t calculate_crc32(uint8_t *message)
 //{
-//  struct netconn *conn, *newconn;
-//  err_t err;
-//  LWIP_UNUSED_ARG(arg);
+//	/* CRC data */
+//	CRC_Type *base = CRC0;
+//	static uint32_t checksum32;
 //
-//  size_t test_string_len, padded_len;
-//  uint8_t *enc_msg;
-//  uint8_t *dec_msg;
+//	size_t len;
 //
-//  /* CRC data */
-//  CRC_Type *base = CRC0;
-//  uint32_t checksum32;
+//	len = strlen(message);
 //
-//  /* Create a new connection identifier. */
-//  /* Bind connection to well known port number 7. */
-//#if LWIP_IPV6
-//  conn = netconn_new(NETCONN_TCP_IPV6);
-//  netconn_bind(conn, IP6_ADDR_ANY, 7);
-//#else /* LWIP_IPV6 */
-//  conn = netconn_new(NETCONN_TCP);
-//  netconn_bind(conn, IP_ADDR_ANY, 7);
-//#endif /* LWIP_IPV6 */
-//  LWIP_ERROR("tcpecho: invalid conn", (conn != NULL), return;);
+//	InitCrc32(base, 0xFFFFFFFFU);
+//	CRC_WriteData(base, (uint8_t *)&message[0], len-4);
+//	checksum32 = CRC_Get32bitResult(base);
 //
-//  /* Tell connection to go into listening mode. */
-//  netconn_listen(conn);
+//	return checksum32;
 //
-//  while (1) {
-//
-//    /* Grab new connection. */
-//    err = netconn_accept(conn, &newconn);
-//    /*printf("accepted new connection %p\n", newconn);*/
-//    /* Process the new connection. */
-//    if (err == ERR_OK) {
-//      struct netbuf *buf;
-//      void *data;
-//      u16_t len;
-//
-//      while ((err = netconn_recv(newconn, &buf)) == ERR_OK) {
-//        /*printf("Received\n");*/
-//        do {
-//
-//        	netbuf_data(buf, &data, &len);
-//
-////        	/*Check CRC32*/
-////        	InitCrc32(base, 0xFFFFFFFFU);
-////        	CRC_WriteData(base, (uint8_t *)&padded_msg[0], padded_len);
-////        	checksum32 = CRC_Get32bitResult(base);
-////        	/*Decrypt message*/
-////		    decrypted_message =  decrypt(uint8_t *message);
-//
-//
-//
-//             err = netconn_write(newconn, data, len, NETCONN_COPY);
-//#if 0
-//            if (err != ERR_OK) {
-//              printf("tcpecho: netconn_write: error \"%s\"\n", lwip_strerr(err));
-//            }
-//#endif
-//        } while (netbuf_next(buf) >= 0);
-//        netbuf_delete(buf);
-//      }
-//      /*printf("Got EOF, looping\n");*/
-//      /* Close connection and discard connection identifier. */
-//      netconn_close(newconn);
-//      netconn_delete(newconn);
-//    }
-//  }
 //}
-///*-----------------------------------------------------------------------------------*/
-//
+
+void tcpecho_server_ssnet_thread(void *arg)
+{
+  struct netconn *conn, *newconn;
+  err_t err;
+  LWIP_UNUSED_ARG(arg);
+
+  uint8_t *enc_msg;
+  uint8_t *dec_msg;
+
+  /* CRC data */
+  uint32_t checksum32;
+
+  /* Create a new connection identifier. */
+  /* Bind connection to well known port number 7. */
+#if LWIP_IPV6
+  conn = netconn_new(NETCONN_TCP_IPV6);
+  netconn_bind(conn, IP6_ADDR_ANY, 7);
+#else /* LWIP_IPV6 */
+   conn = netconn_new(NETCONN_TCP);
+  netconn_bind(conn, IP_ADDR_ANY, 7);
+#endif /* LWIP_IPV6 */
+  LWIP_ERROR("tcpecho: invalid conn", (conn != NULL), return;);
+
+  /* Tell connection to go into listening mode. */
+  netconn_listen(conn);
+
+  while (1) {
+
+    /* Grab new connection. */
+    err = netconn_accept(conn, &newconn);
+//    PRINTF("Accepted new connection %p\r\n", newconn);
+
+    /* Process the new connection. */
+    if (err == ERR_OK) {
+      struct netbuf *buf;
+      void *data;
+      uint8_t *data_msg;
+      u16_t len;
+      //uint8_t message[1024];
+	  uint8_t chksum[4];
+      uint8_t *p_chksum = chksum;
+//      uint32_t *p_checksum32 = &checksum32;
+      uint32_t chksum_bytes;
+      size_t org_len;
+
+
+      while ((err = netconn_recv(newconn, &buf)) == ERR_OK) {
+//        PRINTF("Received\r\n");
+
+        do {
+
+        	netbuf_data(buf, &data, &len);
+
+        	/*Get the message body*/
+        	data_msg =(uint8_t *) data;
+
+        	uint8_t message[len-4];
+        	uint8_t *p_message = message;
+
+        	PRINTF("Received data: \r\n");
+        	for(int i=0; i<len; i++) {
+        	    PRINTF("0x%02x,", data_msg[i]);
+        			}
+        	PRINTF("\r\n");
+
+        	/*Get body and CRC part*/
+        	uint8_t k = 3;
+        	for (int i=0; i<len; i++)
+        	{
+        		if (i<(len-4))
+        		p_message[i] = data_msg[i];
+        		else
+        		{
+        		p_chksum[k] = data_msg[i];
+        		k--;
+        		}
+        	}
+
+        	chksum_bytes = chksum[3]| (chksum[2]<<8 | chksum[1] <<16 | chksum[0] <<24);
+
+        	PRINTF("Message body: \r\n");
+          	for(int i=0; i<len-4; i++) {
+               	PRINTF("0x%02x,", p_message[i]);
+                }
+            PRINTF("\r\n");
+
+            PRINTF("CHKSUM: ");
+            for(int i=0; i<4; i++) {
+            PRINTF("0x%02x,", p_chksum[i]);
+            }
+            PRINTF("\r\n");
+            PRINTF("Checksum bytes: 0x%08x\r\n", chksum_bytes);
+
+
+        	/*Calculate CRC32 for body message*/
+        	InitCrc32(base, 0xFFFFFFFFU);
+        	CRC_WriteData(base, (uint8_t *)&p_message[0], len-4);
+        	checksum32 = CRC_Get32bitResult(base);
+
+
+            //checksum32 = calculate_crc32(p_message);
+        	PRINTF("CRC-32: 0x%08x\r\n", checksum32);
+
+        	/*Compare calculated checksum with the one contained in the message*/
+        	if (chksum_bytes != checksum32)
+        	{
+        		PRINTF("\n Checksum error! \n");
+        	}
+        	else
+        	{
+           	    /*Decrypt message and send it back to client*/
+        	    dec_msg = decrypt(p_message);
+
+        	    org_len = strlen(dec_msg);
+
+        		/*Calculate original length*/
+        		for (int i=0; i<len; i++)
+        		{
+        			if (dec_msg[i] == 0)
+        			{
+        				org_len = i;
+        			    break;
+        			}
+        		}
+
+        		uint8_t decrypt_msg[org_len];
+
+        	    memcpy(decrypt_msg, dec_msg, org_len);
+
+        		/*Encrypt message again*/
+        	    enc_msg = encrypt(decrypt_msg);
+
+        	    /*Calculate CRC32 of encrypted message*/
+
+//        	    checksum32 = calculate_crc32(enc_msg);
+        		InitCrc32(base, 0xFFFFFFFFU);
+        		CRC_WriteData(base, (uint8_t *)&enc_msg[0], org_len);
+        		checksum32 = CRC_Get32bitResult(base);
+
+        	    /*Concatenate encrypted message and checksum*/
+        	    data = enc_msg + checksum32;
+
+        	    /*Send data*/
+        	    err = netconn_write(newconn, data, len, NETCONN_COPY);
+        	}
+
+#if 0
+            if (err != ERR_OK) {
+              printf("tcpecho: netconn_write: error \"%s\"\n", lwip_strerr(err));
+            }
+#endif
+        } while (netbuf_next(buf) >= 0);
+        netbuf_delete(buf);
+      }
+      /*printf("Got EOF, looping\n");*/
+      /* Close connection and discard connection identifier. */
+      netconn_close(newconn);
+      netconn_delete(newconn);
+    }
+  }
+}
+/*-----------------------------------------------------------------------------------*/
+
